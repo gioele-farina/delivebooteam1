dd6f094219ebb14ed39f8ca9de251a8f3f530aef
sourceCodeISA <isabel.gogov1@libero.it>
2021-03-04 11:24:39 +0100

    modifiche

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,7 +60,9 @@
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        <div>
+                                            {{ __('Forgot Your Password?') }}
+                                        </div>
                                     </a>
                                 @endif
                             
